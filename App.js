34381ba73b1b01f0b94aa7d5b4c2470f44ef6f7e
Jamila <j.r.cba@hotmail.com>
2024-02-27 19:03:44 +0300

    pushTopull

--- App.js
+++ App.js
@@ -2,11 +2,16 @@ import { NavigationContainer } from "@react-navigation/native";
 import AuthNavigation from "./src/navigations/main/AuthNavigation";
 import History from "./src/screens/helper/History/History";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import UserProfile from "./src/screens/user/Profile/UserProfile";
+import React from "react";
+import { NativeBaseProvider, Box, Text } from "native-base";
 
 export default function App() {
   return (
     <QueryClientProvider client={new QueryClient()}>
-      <History />
+      <NativeBaseProvider>
+        <History />
+      </NativeBaseProvider>
     </QueryClientProvider>
     // <NavigationContainer>
     //   <AuthNavigation />
