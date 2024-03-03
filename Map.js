cbe29a7af6e64a378d59194fe9dcc36124746b9f
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2024-03-03 15:59:03 +0300

    aa

---
+++ Map.js
@@ -0,0 +1,102 @@
+import React, { useEffect, useRef, useState } from "react";
+import MapView, { Circle, Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import { Button, StyleSheet, View } from "react-native";
+import * as Location from "expo-location";
+
+const Mapz = () => {
+  const [location, setLocation] = useState(null);
+  const mapRef = useRef(null);
+  const [lock, setLock] = useState(false);
+  const [zoomInfo, setZoomInfo] = useState({});
+  useEffect(() => {
+    let intervalId;
+
+    const fetchLocation = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
+        return;
+      }
+      const loc = await Location.getCurrentPositionAsync({});
+      setLocation(loc);
+    };
+
+    fetchLocation(); // Fetch immediately on component mount
+
+    intervalId = setInterval(() => {
+      fetchLocation(); // Fetch every second
+    }, 100);
+
+    return () => {
+      clearInterval(intervalId); // Clear interval on component unmount
+    };
+  }, []);
+
+  // Ensure latitude and longitude are not undefined before rendering the map
+  const latitude = location?.coords?.latitude;
+  const longitude = location?.coords?.longitude;
+
+  useEffect(() => {
+    const newRegion = {
+      latitude: latitude,
+      longitude: longitude,
+      latitudeDelta: zoomInfo.latitudeDelta,
+      longitudeDelta: zoomInfo.longitudeDelta,
+    };
+    if (mapRef) {
+      if (lock) mapRef.current.animateToRegion(newRegion, 0);
+    }
+  }, [location]);
+
+  return latitude && longitude ? (
+    <>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        ref={mapRef}
+        onRegionChange={(r) => {
+          setZoomInfo({
+            ...zoomInfo,
+            latitudeDelta: r.latitudeDelta,
+            longitudeDelta: r.longitudeDelta,
+          });
+        }}
+        initialRegion={{
+          latitude: latitude,
+          longitude: longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          onPress={() => {
+            setLock(!lock);
+          }}
+          coordinate={{ latitude: latitude, longitude: longitude }}
+        />
+        <Circle
+          center={{ latitude: latitude, longitude: longitude }}
+          radius={200}
+          strokeWidth={2}
+          strokeColor="#3399ff"
+          fillColor="#80bfff"
+        />
+
+        {/* <Button></Button> */}
+      </MapView>
+      <Button
+        title="Press me"
+        onPress={() => Alert.alert("Simple Button pressed")}
+      />
+    </>
+  ) : null;
+};
+
+export default Mapz;
+
+const styles = StyleSheet.create({
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
